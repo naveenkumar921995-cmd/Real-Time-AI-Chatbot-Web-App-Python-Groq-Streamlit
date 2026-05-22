4937784de14265443ca169eade707159d77efbdc
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-05-22 21:49:51 +0530

    Update voice.py

--- components/voice.py
+++ components/voice.py
@@ -1,10 +1,14 @@
+# ==============================
+# components/voice.py
+# ==============================
+
 from streamlit_mic_recorder import mic_recorder
 
 def voice_input():
 
     audio = mic_recorder(
-        start_prompt="🎤 Start",
-        stop_prompt="⏹ Stop",
+        start_prompt="🎤 Start Recording",
+        stop_prompt="⏹ Stop Recording",
         key='recorder'
     )
 
