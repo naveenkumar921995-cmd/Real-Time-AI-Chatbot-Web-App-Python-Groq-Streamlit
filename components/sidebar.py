644443da4bbbc8ef1b2aa5293e1985517d3dafa1
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-05-22 21:44:33 +0530

    Create sidebar.py

---
+++ components/sidebar.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def sidebar_settings():
+
+    st.sidebar.title("⚙ Settings")
+
+    model = st.sidebar.selectbox(
+        "Choose AI Model",
+        [
+            "llama-3.3-70b-versatile",
+            "mixtral-8x7b-32768",
+            "gemma2-9b-it"
+        ]
+    )
+
+    temperature = st.sidebar.slider(
+        "Temperature",
+        0.0,
+        1.0,
+        0.7
+    )
+
+    dark_mode = st.sidebar.toggle("Dark Mode")
+
+    return model, temperature, dark_mode
