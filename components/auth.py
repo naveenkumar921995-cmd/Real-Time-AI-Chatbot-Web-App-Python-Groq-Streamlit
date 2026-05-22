1786a67b60324f327f29012588d8cf931f6ec722
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-05-22 21:46:05 +0530

    Create auth.py

---
+++ components/auth.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def login():
+
+    st.sidebar.title("🔐 Login")
+
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input(
+        "Password",
+        type="password"
+    )
+
+    if st.sidebar.button("Login"):
+
+        if username == "admin" and password == "1234":
+
+            st.session_state.logged_in = True
+
+            st.success("Login Successful")
+
+        else:
+
+            st.error("Invalid Credentials")
